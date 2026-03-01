280b8c1fd78588e644d121d6acc13fb615617dd5
Lawrence Liu <smartlitchi@gmail.com>
2026-03-01 09:23:31 +0000

    fix: version.json bypass SW cache - so update settings page shows correct commit info

--- src/webpage/service.ts
+++ src/webpage/service.ts
@@ -302,7 +302,7 @@ self.addEventListener("fetch", async (e) => {
 		return;
 	}
 	const pathname = new URL(req.url).pathname;
-	if (pathname.startsWith("/api/") || pathname === "/getupdates" || pathname === "/reset") {
+	if (pathname.startsWith("/api/") || pathname === "/getupdates" || pathname === "/version.json" || pathname === "/reset") {
 		return;
 	}
 	try {
