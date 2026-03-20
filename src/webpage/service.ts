135a3ecfed9c7638360fa2794ea4a8968c28d808
Lawrence Liu <smartlitchi@gmail.com>
2026-03-20 12:35:26 +0000

    revert: allow SW to cache manifest.json and pwa-icon files again

--- src/webpage/service.ts
+++ src/webpage/service.ts
@@ -302,7 +302,7 @@ self.addEventListener("fetch", async (e) => {
 		return;
 	}
 	const pathname = new URL(req.url).pathname;
-	if (pathname.startsWith("/api/") || pathname === "/getupdates" || pathname === "/version.json" || pathname === "/user-defaults.json" || pathname === "/reset" || pathname === "/manifest.json" || pathname.startsWith("/pwa-icon-")) {
+	if (pathname.startsWith("/api/") || pathname === "/getupdates" || pathname === "/version.json" || pathname === "/user-defaults.json" || pathname === "/reset") {
 		return;
 	}
 	try {
